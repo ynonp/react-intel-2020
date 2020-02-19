ea4c4a260c383d3d24da7130807a8fc98d2b70d6
ynonp <ynonperek@gmail.com>
2020-02-19 12:46:03 +0200

    new router demo files

--- router-demo/src/App.tsx
+++ router-demo/src/App.tsx
@@ -1,24 +1,89 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link, useParams
+} from "react-router-dom";
+
 import './App.css';
+import {number} from "prop-types";
+
+
+function Users() {
+    const { id } = useParams();
+
+    const index = Number(id) - 1;
+
+    const users = [
+        { name: 'ynon' },
+        { name: 'demo' },
+        { name: 'bill' },
+    ];
+
+    return (
+        <div>
+            <p>Hello {users[index].name}</p>
+        </div>
+    )
+}
+
+function Home() {
+    return (
+        <div>
+            <p>Home Page</p>
+        </div>
+    )
+}
+
+
+function About() {
+    return (
+        <div>
+            <p>About Page</p>
+        </div>
+    )
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+        <div className="App">
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/users/1">Users/1</Link>
+                                <Link to="/users/2">Users/2</Link>
+                                <Link to="/users/3">Users/3</Link>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                    <Switch>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/users/:id">
+                            <Users />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+
     </div>
   );
 }
